7c938cad84e82f953ae325882fc176c97b3cddc5
jaravellzie <53119278+jaravellzie@users.noreply.github.com>
2019-09-16 11:05:02 +0800

    Add files via upload

---
+++ LectureSeven.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html>
+<head>
+	<title>Responsive Web-Design</title>
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="index-style-1.css"/>
+	<link rel="stylesheet" href="index-style-2.css" media="screen and (max-width: 900px)"/>
+	<link rel="stylesheet" href="index-style-3.css" media="screen and (max-width: 600px)"/>
+
+</head>
+
+<body>
+
+<header>
+	<h1><center>My Website</center></h1>
+</header>
+
+<nav>
+	<ul>
+		<li><a href="LectureSeven.php">Home</a></li>
+		<li><a href="LectureSeven.php">About</a></li>
+		<li><a href="LectureSeven.php">Link</a></li>
+		<li><a href="LectureSeven.php">Contact</a></li>
+	</ul>
+</nav>
+
+<div id="article1">
+	<img src="Baguio.jpg"/>
+	<p>
+	I'm a paragraph. Click here to add your own text and edit me. It’s easy. 
+	Just click “Edit Text” or double click me and you can start adding your own 
+	content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.
+	</p>
+</div>
+<div id="article2">
+	<img src="Batangas.jpg"/>
+	<p>
+	I'm a paragraph. Click here to add your own text and edit me. It’s easy. 
+	Just click “Edit Text” or double click me and you can start adding your own 
+	content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.
+	</p>
+</div>
+<div id="article3">
+	<img src="Cebu.jpg"/>
+	<p>
+	I'm a paragraph. Click here to add your own text and edit me. It’s easy. 
+	Just click “Edit Text” or double click me and you can start adding your own 
+	content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.
+	</p>
+</div>
+<div id="article4">
+	<img src="Zambales.jpg"/>
+	<p>
+	I'm a paragraph. Click here to add your own text and edit me. It’s easy. 
+	Just click “Edit Text” or double click me and you can start adding your own 
+	content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.
+	</p>
+</div>
+
+
+
+</body>
+</html>
